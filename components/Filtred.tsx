4da10685dd21719d711ef8230abc5511ae587adc
mrxonex <mikhail.poleshchenkov@mail.ru>
2022-11-03 15:45:58 +0300

    update

--- components/Filtred.tsx
+++ components/Filtred.tsx
@@ -3,7 +3,7 @@ import { AiOutlineArrowDown, AiOutlineArrowUp } from 'react-icons/ai'
 
 export default function Filtred() {
   return (
-    <div className="rounded-[10px] p-[15px] bg-white shadow-jj w-[330px] flex flex-col divide-y">
+    <div className="rounded-[10px] p-[15px] bg-white shadow-jj w-[280px] flex flex-col divide-y">
 <div className="flex mt-[-5px] py-[7px] cursor-pointer justify-between">
         <span className="text-[#292928] font-medium text-[20px]">Цена</span>
         <button className='hover:bg-[#E7E9EB] p-[3px] rounded-[10px]'>
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,9 +8,19 @@ export default function Home() {
     <Container>
       <Header />
       <div className="flex pt-[50px] justify-between">
-        <Filtred />
+        <div className="w-[270px]">
+          <Filtred />
+        </div>
         <div className="flex ml-[34px] w-full flex-col">
-          <div className="grid xl:grid-cols-4 grid-cols-3 gap-5">
+          <div className="shadow-jj text-[18px] font-medium flex justify-evenly py-[20px] p-[15px] w-full rounded-[10px] bg-white">
+            <button>Все</button>
+            <button>Кроссовки</button>
+            <button>Худи</button>
+            <button>Джерси</button>
+            <button>Шорты</button>
+            <button>Куртки</button>
+          </div>
+          <div className="grid mt-[30px] xl:grid-cols-4 grid-cols-3 gap-5">
             <Card /> <Card /> <Card /> <Card />
           </div>
         </div>
