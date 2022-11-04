0531c52291a2f5bd0e026be6be5a8c5829374b9d
mrxonex <mikhail.poleshchenkov@mail.ru>
2022-11-04 20:46:43 +0300

    update

--- components/MobileMenu.tsx
+++ components/MobileMenu.tsx
@@ -4,7 +4,7 @@ import { FaUserAlt, FaShoppingBasket } from 'react-icons/fa'
 
 export default function MobileMenu() {
     return (
-        <div className="shadow-jj md:hidden flex justify-around items-center px-[20px] h-[55px] rounded-[10px] bg-white w-[95%] fixed bottom-[20px] left-[2.5%]">
+        <div className="shadow-jj md:hidden flex justify-around items-center px-[20px] h-[55px] rounded-[10px] bg-white w-[95%] fixed bottom-[10px] left-[2.5%]">
             <button className='hover:bg-[#8045C61A] rounded-[10px] p-[5px]'>
                 <HiHome color="#a67fd4" size={32} />
             </button>
