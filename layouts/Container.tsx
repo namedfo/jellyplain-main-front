e5ab90c229261477044a33053f02cf2f7571f4f1
mrxonex <mikhail.poleshchenkov@mail.ru>
2022-11-04 10:30:07 +0300

    update

--- layouts/Container.tsx
+++ layouts/Container.tsx
@@ -1,7 +1,17 @@
+import Head from "next/head";
+//
+import logo from '../utils/logo.jpg'
+
 export default function Container({ children }: any) {
-    return (
-        <div className="mx-auto pt-[25px] px-[20px] xl:p-0 xl:pt-[25px] h-full max-w-[1280px]">
-            {children}
-        </div>
-    )
+  return (
+    <div className="mx-auto pt-[25px] px-[20px] xl:p-0 xl:pt-[25px] h-full max-w-[1280px]">
+      <Head>
+        <title>JellyPlain</title>
+
+        <link rel="shortcut icon" href="https://i.imgur.com/HpmgXDo.png" />
+        <meta property="og:title" content="JellyPlain" key="title" />
+      </Head>
+      <div className="h-full">{children}</div>
+    </div>
+  );
 }
