77ce8abaf38d4980a3738be108b5d46175c66e91
mrxonex <mikhail.poleshchenkov@mail.ru>
2022-11-11 14:59:29 +0300

    update

--- pages/redirect.tsx
+++ pages/redirect.tsx
@@ -1,23 +1,33 @@
+import axios from "axios";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 // layouts
 import Container from "../layouts/Container";
 
 export default function Redirect() {
+  const router = useRouter();
 
-    const router = useRouter()
-    const dirtyData = router.asPath.split(/[&\/=]/)
-    const data = {
+  useEffect(() => {
+    (async () => {
+      const dirtyData = router.asPath.split(/[&\/=]/);
+      const data: any = {
         access_token: dirtyData[2],
         expires_in: dirtyData[4],
-        user_id: dirtyData[6]
-    }
-
-    console.log(data)
-    return (
-        <Container>
-            <div className="w-full h-full flex items-center justify-center">
-                Auth in process...
-            </div>
-        </Container>
-    )
+        user_id: dirtyData[6],
+      };
+      try {
+        const res = await axios.get(
+          `https://api.vk.com/method/users.get?user_ids=${data.user_id}&fields=photo_200,bdate&access_token=${data.access_token}&v=5.131`
+        );
+        console.log(res)
+      } catch (error) {}
+    })();
+  }, []);
+  return (
+    <Container>
+      <div className="w-full h-full flex items-center justify-center">
+        Auth in process...
+      </div>
+    </Container>
+  );
 }
