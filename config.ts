c74ab637573a1f2c5e532acbfd4c820224b49da6
mrxonex <mikhail.poleshchenkov@mail.ru>
2022-11-13 13:17:27 +0300

    update

--- config.ts
+++ config.ts
@@ -5,7 +5,7 @@ const $api = axios.create({
 });
 
 $api.interceptors.request.use((config: any) => {
-  config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`
+  config.headers.Authorization = `Bearer ${localStorage.getItem('jjwt')}`
   return config;
 })
 
