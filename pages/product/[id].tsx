cf75d9c5902e2dbf6bab1ec1c84d39b3e1d36a66
mrxonex <mikhail.poleshchenkov@mail.ru>
2022-11-13 20:35:30 +0300

    s

--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -1,15 +1,17 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+//
 import ImageGallery from "react-image-gallery";
 //
-import { AiOutlinePlus, AiOutlineMinus, AiFillStar } from "react-icons/ai";
+import { AiFillStar } from "react-icons/ai";
 import { BiCommentDots } from "react-icons/bi";
 // components
 import Header from "../../components/Header";
+import LLoading from "../../components/Loading";
 // layouts
 import Container from "../../layouts/Container";
-import { useEffect, useState } from "react";
-import axios from "axios";
+// hooks
 import useActions from "../../hooks/useActions";
-import LLoading from "../../components/Loading";
 
 const images = [
   {
@@ -34,21 +36,22 @@ const images = [
 
 export default function Product() {
   const [product, setProduct] = useState<any>(undefined);
-  const [isLoading, setIsLoading] = useState<any>("idle");
+  const [productChild, setProductChild] = useState<any>(undefined);
 
-  const { setCard } = useActions();
+  const [isLoading, setIsLoading] = useState<any>("idle");
 
   useEffect(() => {
     (async () => {
-      setIsLoading("loading")
+      setIsLoading("loading");
       try {
         const res = await axios.get(
           `https://jellyplainv2.herokuapp.com/product/getOne?id=${1}`
         );
         setProduct(res.data);
-        setIsLoading("success")
+        setProductChild(res.data.productChilds[0]);
+        setIsLoading("success");
       } catch (error) {
-        setIsLoading("error")
+        setIsLoading("error");
       }
     })();
   }, []);
@@ -63,57 +66,55 @@ export default function Product() {
   return (
     <Container>
       <Header />
-      {isLoading === 'loading' ? (
+      {isLoading === "loading" ? (
         <div className="flex h-full items-center justify-center">
           <LLoading />
         </div>
       ) : (
         <div className="flex md:pt-[50px] h-full flex-col">
-          <div className="flex justify-between mt-[25px]">
-            <div className="shadow-jj w-[600px] flex justify-center rounded-[10px] bg-white p-[15px]">
-              {product?.productChilds[0].images && (
+          <div className="flex flex-col xl:flex-row justify-between sm:mt-[25px]">
+            <div className="shadow-jj w-full sm:w-[600px] flex justify-center rounded-[10px] bg-white p-[15px]">
+              <div className="hidden sm:flex">
+                {productChild.images && (
                 <ImageGallery
                   autoPlay={true}
-                  items={getImgFormat(product?.productChilds[0].images)}
+                  items={getImgFormat(productChild.images)}
                 />
               )}
+              </div>
+              <div className="block sm:hidden">
+                {productChild.images && (
+                  <ImageGallery
+                    showFullscreenButton={false}
+                    showThumbnails={false}
+                    showPlayButton={false}
+                    showBullets={true}
+                    autoPlay={true}
+                    items={getImgFormat(productChild.images)}
+                  />
+                )}
+              </div>
             </div>
             <div>
-              <div className="shadow-jj rounded-[10px] w-[650px] bg-white py-[15px] px-[25px]">
-                <div className="flex justify-between">
-                  <div className="flex flex-col">
-                    <span className="text-[22px] font-medium">
-                      {product?.title}
+              <div className="shadow-jj mt-[25px] sm:mt-0 flex flex-col rounded-[10px] w-[650px] bg-white py-[15px] px-[25px]">
+                <Title product={product} />
+                <div>
+                  <span className="text-[#775C5C] font-medium text-[18px]">
+                    Colors
+                  </span>
+                </div>
+                <div>
+                  <span className="text-[#775C5C] font-medium text-[18px]">
+                    Sizes
+                  </span>
+                </div>
+                <div className="flex flex-col mt-[30px]">
+                  <div className="flex justify-between">
+                    <span className="text-[#4A3333] font-medium text-[20px]">
+                      Comments
                     </span>
-                    <div className="flex justify-start">
-                      <div className="flex cursor-pointer py-[1px] px-[3px] rounded-lg hover:bg-[#8045C61A]">
-                        <div className="flex items-center">
-                          <AiFillStar color="#fbbf24" />
-                          <span className="text-[#79828e] text-[18px] font-medium ml-[5px]">
-                            4.3
-                          </span>
-                        </div>
-                        <div className="flex ml-[10px] items-center">
-                          <BiCommentDots color="#35404A" />
-                          <span className="text-[#79828e] text-[18px] font-medium  ml-[5px]">
-                            53
-                          </span>
-                        </div>
-                      </div>
-                      <span className="font-medium text-[18px] ml-[25px] text-[#5B9F32]">
-                        Оригинал
-                      </span>
-                    </div>
-                  </div>
-                  <div className="flex items-end  flex-col">
-                    <span className="text-[#ffa500] font-medium text-[22px]">
-                      {product?.price} &#8381;
-                    </span>
-                    <button
-                      onClick={() => setCard(product)}
-                      className="bg-[#8a63b9] text-[17px] hover:bg-[#8062a7] text-white font-medium px-[15px] py-[3px] rounded-[10px]"
-                    >
-                      <span>В корзину</span>
+                    <button className="border text-[#8a63b9] hover:bg-[#8045C61A] px-[8px] rounded-[10px] font-medium text-[16px] border-[#8a63b9]">
+                      Оставить отзыв
                     </button>
                   </div>
                 </div>
@@ -125,3 +126,45 @@ export default function Product() {
     </Container>
   );
 }
+
+const Title = ({ product }: any) => {
+  const { setCard } = useActions();
+
+  return (
+    <div className="flex justify-between">
+      <div className="flex flex-col">
+        <span className="text-[22px] font-medium">{product?.title}</span>
+        <div className="flex justify-start">
+          <div className="flex cursor-pointer py-[1px] px-[3px] rounded-lg hover:bg-[#8045C61A]">
+            <div className="flex items-center">
+              <AiFillStar color="#fbbf24" />
+              <span className="text-[#79828e] text-[18px] font-medium ml-[5px]">
+                4.3
+              </span>
+            </div>
+            <div className="flex ml-[10px] items-center">
+              <BiCommentDots color="#35404A" />
+              <span className="text-[#79828e] text-[18px] font-medium  ml-[5px]">
+                53
+              </span>
+            </div>
+          </div>
+          <span className="font-medium text-[18px] ml-[25px] text-[#5B9F32]">
+            Оригинал
+          </span>
+        </div>
+      </div>
+      <div className="flex items-end  flex-col">
+        <span className="text-[#ffa500] font-medium text-[22px]">
+          {product?.price} &#8381;
+        </span>
+        <button
+          onClick={() => setCard(product)}
+          className="bg-[#8a63b9] text-[17px] hover:bg-[#8062a7] text-white font-medium px-[15px] py-[4px] rounded-[10px]"
+        >
+          <span>В корзину</span>
+        </button>
+      </div>
+    </div>
+  );
+};
