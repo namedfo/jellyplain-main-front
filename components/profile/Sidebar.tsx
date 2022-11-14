5ede82475e0cc3d72b46883a2482307d58477092
mrxonex <mikhail.poleshchenkov@mail.ru>
2022-11-14 11:28:29 +0300

    s

--- components/profile/Sidebar.tsx
+++ components/profile/Sidebar.tsx
@@ -4,7 +4,7 @@ import useActions from "../../hooks/useActions";
 import { useTypedSelector } from "../../hooks/useTypedSelector";
 
 export default function Sidebar() {
-  const { isAuth, user, isLoading } = useTypedSelector((state) => state.user);
+  const { user, isLoading } = useTypedSelector((state) => state.user);
 
   const { setUser, setIsAuth } = useActions();
 
@@ -21,48 +21,46 @@ export default function Sidebar() {
   };
   return (
     <>
-      {isAuth && isLoading === 'success' && (
-        <div className="flex flex-col md:flex-row w-full justify-between">
-          <div className="flex flex-col">
-            <div className="shadow-jj relative p-[15px] flex flex-col items-center rounded-[10px] bg-white w-full sm:w-[240px]">
-              {user?.avatar_url ? (
-                <img
-                  className="border rounded-full w-[120px] h-[120px]"
-                  src={user?.avatar_url}
-                  alt="avatar"
-                />
-              ) : (
-                <div className="border bg-[whitesmoke] text-[#5e5850] text-[28px] rounded-full flex items-center justify-center w-[120px] h-[120px]">
-                  <span>{"M".charAt(0)}</span>
-                </div>
-              )}
-              <div className="flex mt-[12px] text-[20px] items-center">
-                <span>{user?.first_name}</span>
-                <span className="ml-[8px]">{user?.last_name}</span>
+      <div className="flex flex-col md:flex-row w-full justify-between">
+        <div className="flex flex-col">
+          <div className="shadow-jj relative p-[15px] flex flex-col items-center rounded-[10px] bg-white w-full sm:w-[240px]">
+            {user?.avatar_url ? (
+              <img
+                className="border rounded-full w-[120px] h-[120px]"
+                src={user?.avatar_url}
+                alt="avatar"
+              />
+            ) : (
+              <div className="border bg-[whitesmoke] text-[#5e5850] text-[28px] rounded-full flex items-center justify-center w-[120px] h-[120px]">
+                <span>{"M".charAt(0)}</span>
               </div>
-              <button
-                onClick={onHandleExit}
-                className="absolute font-medium text-[#A53D3D] right-[15px] top-[5px]"
-              >
-                выйти
-              </button>
+            )}
+            <div className="flex mt-[12px] text-[20px] items-center">
+              <span>{user?.first_name}</span>
+              <span className="ml-[8px]">{user?.last_name}</span>
             </div>
-            <div className="relative shadow-jj mt-[30px] p-[15px] flex flex-row sm:flex-col items-center rounded-[10px] bg-white w-full sm:w-[240px]">
-              <span className="absolute text-[#444f58] left-[15px] top-[3px]">
-                меню
-              </span>
-              <button className="text-[#86368d] mt-[20px] sm:mt-[15px] w-[98%] hover:bg-[#8A63B91A] rounded-[10px] py-[5px] font-medium text-[18px] sm:text-[20px]">
-                Заказы
-              </button>
-            </div>
+            <button
+              onClick={onHandleExit}
+              className="absolute font-medium text-[#A53D3D] right-[15px] top-[5px]"
+            >
+              выйти
+            </button>
           </div>
-          <div className="w-full flex justify-end">
-            <div>
-              <div className="shadow-jj p-[15px] flex flex-col items-center rounded-[10px] bg-white w-[1000px]"></div>
-            </div>
+          <div className="relative shadow-jj mt-[30px] p-[15px] flex flex-row sm:flex-col items-center rounded-[10px] bg-white w-full sm:w-[240px]">
+            <span className="absolute text-[#444f58] left-[15px] top-[3px]">
+              меню
+            </span>
+            <button className="text-[#86368d] mt-[20px] sm:mt-[15px] w-[98%] hover:bg-[#8A63B91A] rounded-[10px] py-[5px] font-medium text-[18px] sm:text-[20px]">
+              Заказы
+            </button>
           </div>
         </div>
-      )}
+        <div className="w-full flex justify-end">
+          <div>
+            <div className="shadow-jj p-[15px] flex flex-col items-center rounded-[10px] bg-white w-[1000px]"></div>
+          </div>
+        </div>
+      </div>
       {isLoading === "error" && (
         <div
           style={{
--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,15 +1,21 @@
 // components
 import Header from "../components/Header";
 import Sidebar from "../components/profile/Sidebar";
+import { useTypedSelector } from "../hooks/useTypedSelector";
 // layouts
 import Container from "../layouts/Container";
 
 export default function Profile() {
+
+    const { isAuth } = useTypedSelector(state => state.user)
+
     return (
         <Container>
             <Header />
             <div className="pt-0 sm:pt-[50px] h-full">
-                <Sidebar />
+                {isAuth && (
+                    <Sidebar />
+                )}
             </div>
         </Container>
     )
