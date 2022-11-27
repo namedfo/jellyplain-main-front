e11b2d1a7b7fa70bfa54befa5de0e2969f262020
mrxonex <mikhail.poleshchenkov@mail.ru>
2022-11-27 14:11:40 +0300

    update

--- modals/OrderDetail.tsx
+++ modals/OrderDetail.tsx
@@ -23,29 +23,32 @@ Modal.setAppElement("#__next");
 
 export default function OrderDetail({ isOpen, onClose }: any) {
   return (
-    <Modal
-      isOpen={isOpen}
-      style={customStyles}
-      onRequestClose={onClose}
-    >
-      <div>
-         <div className="flex text-[7px] sm:text-[12px] justify-between">
-            <div className="flex text-gray-400 items-center">
-              <span>Оплачено</span>
-              <BsArrowRightShort size={22} />
-            </div>
-            <div className="flex text-red-600 items-center">
-              <span>Ожидает подтверждения</span>
-              <BsArrowRightShort size={22} />
-            </div>
-            <div className="flex text-gray-400 items-center">
-              <span>Подтвержденно</span>
-              <BsArrowRightShort size={22} />
-            </div>
-            <div className="flex text-gray-400 items-center">
-              <span>В пути</span>
-            </div>
+    <Modal isOpen={isOpen} style={customStyles} onRequestClose={onClose}>
+      <div className="flex flex-col">
+        <div className="flex text-[7px] sm:text-[12px] justify-between">
+          <div className="flex text-gray-400 items-center">
+            <span>Оплачено</span>
+            <BsArrowRightShort size={22} />
           </div>
+          <div className="flex text-red-600 items-center">
+            <span>Ожидает подтверждения</span>
+            <BsArrowRightShort size={22} />
+          </div>
+          <div className="flex text-gray-400 items-center">
+            <span>Подтвержденно</span>
+            <BsArrowRightShort size={22} />
+          </div>
+          <div className="flex text-gray-400 items-center">
+            <span>В пути</span>
+          </div>
+        </div>
+        <div className="flex flex-col text-[16px] sm:flex-row my-[3px] sm:items-center justify-between">
+          <span className="mr-[25px]">Заказ создан: 21.01.2022</span>
+          <div className="flex items-center">
+            <span className="block mr-[5px] sm:hidden">Номер заказа:</span>
+            <span>№2929</span>
+          </div>
+        </div>
       </div>
     </Modal>
   );
