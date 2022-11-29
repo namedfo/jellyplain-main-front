1c07d095f302ff7797b41d9f7ab0913a268bd9c0
mrxonex <mikhail.poleshchenkov@mail.ru>
2022-11-29 15:08:32 +0300

    update

--- pages/order/[id].tsx
+++ pages/order/[id].tsx
@@ -2,27 +2,34 @@ import axios from "axios";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { BsArrowRightShort } from "react-icons/bs";
+import dayjs from "dayjs";
+import "dayjs/locale/ru";
 import Header from "../../components/Header";
 import $api from "../../config";
 import useActions from "../../hooks/useActions";
 import Container from "../../layouts/Container";
+dayjs.locale("ru");
 
 export default function Order() {
-  const [orderLocal, setOrderLocal] = useState<any>()
+  const [orderLocal, setOrderLocal] = useState<any>();
 
-  const { setOrder } = useActions()
+  const { setOrder } = useActions();
 
-  const router = useRouter()
+  const router = useRouter();
 
   useEffect(() => {
     (async () => {
       const res = await $api.get(`order/getOne?id=${router.query?.id}`);
-      
-      setOrderLocal(res.data)
-      setOrder(res.data)
-    })()
-  }, [])
 
+      const order = res?.data?.order ? res?.data?.order : res?.data
+      setOrderLocal(order);
+      setOrder(order);
+    })();
+  }, []);
+
+  const date = dayjs(orderLocal?.createdAt).format("DD.MM.YYYY HH:mm");
+
+  console.log(orderLocal);
   return (
     <Container>
       <Header />
@@ -47,8 +54,8 @@ export default function Order() {
           </div>
           <div className="flex flex-col text-[16px] sm:flex-row my-[3px] sm:items-center justify-between">
             <div className="flex items-center">
-            <span className="mr-[5px]">Заказ создан:</span>
-            <span className="mr-[25px]">21.01.2022</span>
+              <span className="mr-[5px]">Заказ создан:</span>
+              <span className="mr-[25px]">{date}</span>
             </div>
             <div className="flex items-center">
               <span className="block mr-[5px] sm:hidden">Номер заказа:</span>
@@ -65,11 +72,44 @@ export default function Order() {
               <span>Ожидает подтвержения</span>
             </div>
           </div>
+          <div className="bg-neutral-100 my-[5px] rounded-[5px] pb-[8px] px-[5px]">
+            <span className="text-[14px] text-slate-900 leading-[5px]">
+              Товары
+            </span>
+            <div className="flex">
+              {/* <div className="h-[50px] rounded-[5px] bg-slate-700 w-[50px]"></div> */}
+              {orderLocal?.productsOrder?.map((productOrder: any) => (
+                <div
+                  key={productOrder.id}
+                  onClick={() => router.push(`/product/${productOrder?.id}`)}
+                  className="relative"
+                >
+                  <img
+                    className="h-[70px] rounded-[10px] border object-cover w-[70px]"
+                    src={productOrder.images[0]}
+                    alt="image"
+                  />
+                  <div className="flex w-full justify-around items-start text-[14px] rounded-[5px] top-0 left-0 absolute">
+                    <span className="font-medium">36</span>
+                    <div>
+                      <div className="px-[15px] rounded-[3px] bg-black py-[5px]" />
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
           <div className="flex flex-col sm:flex-row w-full mt-[20px] justify-between">
-            <button onClick={() => router.push("/")} className="w-full sm:w-[40%] hover:underline text-fuchsia-600 hover:text-fuchsia-700">
+            <button
+              onClick={() => router.push("/")}
+              className="w-full sm:w-[40%] hover:underline text-fuchsia-600 hover:text-fuchsia-700"
+            >
               На главную страницу
             </button>
-            <button onClick={() => router.push("/profile")} className="w-full sm:w-[40%] mt-[10px] sm:mt-0 py-[4px] rounded-[10px] bg-fuchsia-600 hover:bg-fuchsia-700 text-white">
+            <button
+              onClick={() => router.push("/profile")}
+              className="w-full sm:w-[40%] mt-[10px] sm:mt-0 py-[4px] rounded-[10px] bg-fuchsia-600 hover:bg-fuchsia-700 text-white"
+            >
               В личный кабинет
             </button>
           </div>
