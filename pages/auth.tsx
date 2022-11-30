3004be1624c223b2d7a41b1a972e5799deb1858f
mrxonex <mikhail.poleshchenkov@mail.ru>
2022-11-30 15:58:12 +0300

    update

---
+++ pages/auth.tsx
@@ -0,0 +1,18 @@
+import { useRouter } from "next/router"
+import { useEffect } from "react"
+
+export default function Auth() {
+    const router = useRouter()
+
+    useEffect(() => {
+        if (router?.query?.token) {
+            localStorage.setItem("jjwt", router?.query?.token as string)
+            router.push('/')
+
+        }
+    }, [router?.query?.token])
+    
+    return (
+        <div></div>
+    )
+}
